c19bf8421a38aa814934a4aee50ca88a4b3d8c52
Komalasari076 <sitikomala076@gmail.com>
2024-10-17 11:03:05 +0700

    authentication dan authorization

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const todosRoute = require("./todos-route");
+const authRoute = require("./auth-route");
+const { validateToken } = require("../middleware/auth");
+
+router.use((req, res, next) => {
+  console.log("middleware untuk semua route");
+  next();
+});
+
 
 router.get("/", (req, res) => {
   res.json({
@@ -9,5 +17,6 @@ router.get("/", (req, res) => {
   });
 });
 
-router.use("/todos", todosRoute);
+router.use("/todos", validateToken, todosRoute);
+router.use("/auth", authRoute);
 module.exports = router;
